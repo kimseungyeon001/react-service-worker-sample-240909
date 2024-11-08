088a7f22325649c7c088ee810c085e3d15b2ed40
kimseungyeon001 <kim.seungyeon@classmethod.jp>
2024-11-08 21:04:12 +0900

    delete comment

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 // import { certificateFor } from 'devcert'
 
-// NOTE: service worker はhttpsでのみ動く、ただ、localhostの場合はhttpでも問題ない
-// ↓の指定はemulatorで動くケースのために設定している
-// emulatorで動く場合はngrokなどのサービスを利用
 // https://vitejs.dev/config/
 export default defineConfig(async () => {
   // const { key, cert } = await certificateFor('localhost')
